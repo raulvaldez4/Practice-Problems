75f7c3d31e5e8e7205f9315f74b3c0941447053a
JR Valdez <40616631+raulvaldez4@users.noreply.github.com>
2019-01-30 13:00:31 -0600

    Update URLify.py

--- URLify.py
+++ URLify.py
@@ -1,5 +1,7 @@
-#1.3 Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold
-#the add'l characters, and that you are given the 'true' length of the string. 
+# 1.3 Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold
+# the add'l characters, and that you are given the 'true' length of the string. 
+# Input: 'Mr John Smith    ',13
+# Output: 'Mr%20John%20Smith'
 
 def URLify(word,n):
     new_word = []
